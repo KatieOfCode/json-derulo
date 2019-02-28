eef478e303ce546542c53211ffc01c38716fb4ef
Kaitlyn <katiefujihara@gmail.com>
2019-02-28 08:56:16 -0800

    add gif

---
+++ src/pages/index.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+import { jason } from "../images/json.gif"
+
+const IndexPage = () => (
+  <div>
+    <iframe
+      src="https://giphy.com/embed/l41YyrSMvJAwQ57u8"
+      width="480"
+      height="270"
+      frameBorder="0"
+      class="giphy-embed"
+      allowFullScreen
+    />
+    <p>
+      <a href="https://giphy.com/gifs/jasonderulo-l41YyrSMvJAwQ57u8" />
+    </p>
+  </div>
+)
+
+export default IndexPage
